da8ed421f977cabb2a549ccadc019adc2ac6a2df
Brayan Ellis <ellis@localhost.localdomain>
2025-07-08 15:17:56 -0400

    feat(asyncio): agregar manejo de errores en corrutinas con gather y excepciones simuladas
    Se implementó la función descargar_archivo que simula una descarga con fallo aleatorio (25% de probabilidad)usando randint.
    Se usa asyncio.gather con return_exceptions=True para ejecutar múltiples descargas en paralelo sin que una excepción detenga a las demás.
    Se imprimen los resultados, mostrando qué descargas fueron exitosas y cuáles fallaron con su excepción correspondiente.

--- exercise_2.py
+++ exercise_2.py
@@ -15,6 +15,41 @@ Creá una función descargar_archivo(nombre: str) que:
 import asyncio
 from random import randint
 
-archivo_1 = "Can't Stop.mp4"
-archivo_2 = "Aerials.mp4"
-archivo_2 = "The Drug In Me Is You.mp4"
+
+# Definimos la corrutina para simular la descarga de un archivo
+async def descargar_archivo(nombre: str) -> str:
+    print(f'Iniciando la descarga de {nombre}')
+
+    # Simulamos el tiempo de descarga
+    await asyncio.sleep(randint(1, 5))
+
+    # Generamos un número aleatorio que representa la falla del 25% (aprox)
+    if randint(1, 4) == 1:
+        raise Exception(f'Error de red {nombre}')
+    
+    print(f'{nombre} descargado correctamente.')
+    return f'{nombre} OK'
+
+
+# Corrutina principal
+async def main():
+    music = "Can't Stop.mp4"
+    video = "The last of Us - cap 1.mkv"
+    img = "Foto_de_perfil.png"
+
+    # Ejecutamos todas las descargas en paralelo
+    results = await asyncio.gather(
+        descargar_archivo(music),
+        descargar_archivo(video),
+        descargar_archivo(img),
+        return_exceptions=True # para que una excepción no frene a las demás
+    )
+
+    for result in results:
+        if isinstance(result, Exception):
+            print(f'Fallo al descargar {result}')
+        else:
+            print(f'Éxito al descargar {result}')
+
+
+asyncio.run(main())
