a02af8c3768977b4e8152021942034ea50a624c0
Brayan Ellis <ellis@localhost.localdomain>
2025-07-11 16:04:36 -0400

    feat(asyncio): comparar ejecución secuencial vs concurrente con asyncio.create_task y medir tiempo con time.perf_time()

--- exercise_4.py
+++ exercise_4.py
@@ -13,3 +13,32 @@ Objetivo: Comparar concurrencia real.
 
     Imprimí cuánto tiempo te ahorraste al usar concurrencia.
 """
+import asyncio, time
+
+async def proceso_lento(i: int) -> int:
+    await asyncio.sleep(2)
+    return i**2
+
+async def main():
+    # PRIMER MARCADOR -SERIAL-
+    serial_start = time.perf_counter()
+    for n in range(1, 6):
+        await proceso_lento(n)
+    serial_end = time.perf_counter()
+    serial_time = serial_end - serial_start
+    print(f'Primer marcador: {serial_time:.3f}seg')
+
+    # SEGUNDO MARCADOR -CONCURRENTE-
+    concurrent_start = time.perf_counter()
+    procesos = [asyncio.create_task(proceso_lento(n)) for n in range(1, 6)]
+    result = await asyncio.gather(*procesos)
+    concurrent_end = time.perf_counter()
+    concurrent_time = concurrent_end - concurrent_start
+    print(f'Segundo marcador: {concurrent_time:.3f}seg')
+    #FINAL
+
+    # DIFERENCIA DE MARCADORES
+    print(f'Al usar la corrutina me ahorre:')
+    print(f'{serial_time - concurrent_time:.3f}seg')
+
+asyncio.run(main())
