ad3168b117fdbb79bbaa32d20463ddc459e3b7a9
Brayan Ellis <ellis@localhost.localdomain>
2025-07-11 17:07:40 -0400

    feat: se utilizo wait_for() para el manejo de timeout en corrutina

--- exercise_5.py
+++ exercise_5.py
@@ -9,3 +9,19 @@
 
     Si excede el tiempo, capturá la excepción y decí “La API de {nombre} se tardó demasiado”.
 """
+import asyncio
+
+async def consultar_api(nombre: str, tiempo_respuesta: int) -> str:
+    print('Consultando la API. . .')
+    await asyncio.sleep(tiempo_respuesta)
+    return 'OK'
+
+async def main():
+    try:
+        api = 'PokeAPI'
+        # usamos wait_for() para indicar un tiempo de respuesta
+        await asyncio.wait_for(consultar_api(api, 3), 4) # tiempo de espera 4s
+    except asyncio.TimeoutError: # en caso de excederce se lanza una excepción
+        print(f'La API de {api} se tardó demasiado.')
+
+asyncio.run(main())
